820eaecfb2ddd353feadb6c614cc31490cd1a3a0
Alex Kaplan <kaplan2539@gmail.com>
2015-09-14 09:40:37 -0700

    sudo not gksu when killing

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gksudo killall kivy
+sudo killall kivy
 sleep 1
-gksudo killall -9 kivy
+sudo killall -9 kivy
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DISPLAY=:0 gksudo kivy ${DIR}/main.py
