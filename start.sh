a60f7ac2beee12591b4d5b674b3743738c0cf096
Alex Kaplan <kaplan2539@gmail.com>
2015-09-14 09:37:36 -0700

    kill all old kivys before starting a new one

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-DISPLAY=:0 gksudo kivy flasher/main.py
+gksudo killall kivy
+sleep 1
+gksudo killall -9 kivy
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DISPLAY=:0 gksudo kivy ${DIR}/main.py
