8aac53134a11199670236a831810ce630475f25e
Alex Kaplan <kaplan2539@gmail.com>
2015-09-20 10:07:19 -0700

    removed gksu and sudo

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-sudo killall kivy
-sleep 1
-sudo killall -9 kivy
-
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DISPLAY=:0 gksudo kivy ${DIR}/main.py
+DISPLAY=:0 kivy ${DIR}/main.py
