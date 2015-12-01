728c899a71986dda44fd0ab7491b25affc3b5f72
howientc <root@howie>
2015-11-30 18:06:33 -0800

    Moved into separate file

---
+++ flasher/progress.py
@@ -0,0 +1,34 @@
+from kivy.clock import Clock
+class Progress(object):
+    '''
+    Small class to keep track of progress and update progress observers on change
+    '''
+    def __init__(self, progressObservers = [], timeoutObservers = [], start = 0.0, finish=1.0, timeout=None):
+        self.progressObservers = progressObservers
+        self.start = start
+        self.finish = finish
+        self.current = start
+        self.timeout = timeout
+        self.timedOut = False
+        Clock.schedule_interval(self.addProgress.__get__(self,Progress), 1 )
+    
+    def __del__(self):
+        Clock.unschedule(self.addProgress.__get__(self,Progress))
+        
+    def addProgress(self,change):
+        self.setProgress(self.current + change)
+
+    def setProgress(self, value):
+        self.current = value
+        progress = self.getProgress()
+        [observer(progress) for observer in self.progressObservers]
+        if self.timeout:
+            if value >= self.timeout:
+                self.timedOut = True
+                [observer() for observer in self.timeoutObservers]
+                
+
+        
+    def getProgress(self):
+        return self.current / self.finish
+
