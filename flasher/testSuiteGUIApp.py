68431b261b36c54dd2cba7ebb3dfccf33a4be338
howientc <root@howie>
2015-12-14 11:23:52 -0800

    traces

--- flasher/testSuiteGUIApp.py
+++ flasher/testSuiteGUIApp.py
@@ -113,8 +113,8 @@ class TestSuiteGUIApp( App ):
 			currentState = deviceDescriptor.getDeviceState()
 			lastKnownState, when = self.deviceStates[uid]
 			elapsedTime = currentTime - when #see how long its been since we
+			print "state : lastKnown: " + str(lastKnownState) + " current " + str(currentState) + " elapsed " + str(elapsedTime)
 			if lastKnownState != currentState: #if the state is different since last time
-				print "state Change: lastKnown: " + str(lastKnownState) + " current " + str(currentState) + " elapsed " + str(elapsedTime)
 				if (lastKnownState == DEVICE_DISCONNECTED or #if activating, do immediately
 					lastKnownState == DEVICE_FASTBOOT or # if fastboot, process the disconnect
 					(lastKnownState == DEVICE_FEL and currentState == DEVICE_DISCONNECTED and elapsedTime > AUTO_START_WAIT_BEFORE_DISCONNECT )): #handle switch from FEL to FASTBOOT without graying out
