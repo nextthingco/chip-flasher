49cc52ccf334d2759985e2cf46637d83491dd493
howientc <root@howie>
2015-12-14 10:29:30 -0800

    traces

--- flasher/testSuiteGUIApp.py
+++ flasher/testSuiteGUIApp.py
@@ -33,7 +33,7 @@ SORT_HUBS = True # Whether the hub name from the UDEV file (chip_id_hub_xxx) sho
 
 HEADLESS = False # The app can be run headless. This is for a future where we might just use a fixture with LEDs instead of a screen.
 AUTO_START_ON_DEVICE_DETECTION = True #When this is true, the test suite will be run automatically when polling detects device. Button input to start runs is disabled
-AUTO_START_WAIT_BEFORE_DISCONNECT = 15 #wait n seconds before considering a disconnect to handle switch to FASTBOOT
+AUTO_START_WAIT_BEFORE_DISCONNECT = 18 #wait n seconds before considering a disconnect to handle switch to FASTBOOT
 class TestSuiteGUIApp( App ):
 	'''
 	The main application for a GUI-based, parallel test suite runner
@@ -114,6 +114,7 @@ class TestSuiteGUIApp( App ):
 			lastKnownState, when = self.deviceStates[uid]
 			elapsedTime = currentTime - when #see how long its been since we
 			if lastKnownState != currentState: #if the state is different since last time
+				print "state Change: lastKnown: " + str(lastKnownState) + " current " + str(currentState) + " elapsed " + str(elapsedTime)
 				if (lastKnownState == DEVICE_DISCONNECTED or #if activating, do immediately
 					lastKnownState == DEVICE_FASTBOOT or # if fastboot, process the disconnect
 					(lastKnownState == DEVICE_FEL and currentState == DEVICE_DISCONNECTED and elapsedTime > AUTO_START_WAIT_BEFORE_DISCONNECT )): #handle switch from FEL to FASTBOOT without graying out
