f92afd608707b8e169e986e859893d03f67b81f2
howie <howie@nextthing.co>
2017-03-27 17:05:02 -0700

    CHP_FILE_NAME

--- flasher/config.py
+++ flasher/config.py
@@ -1,7 +1,6 @@
 import os
-#specify the .chp file you want to use.
-# CHP_FILE_NAME='/home/debian/Desktop/CHIP-flasher/flash_me.chp'
-CHP_FILE_NAME=os.environ['HOME'] + '/Downloads/stable-chip-pro-blinkenlights-b1-Toshiba_512M_SLC.chp'
+#specify the .chp file you want to use. Can be an absolute path, or using environment variable. 
+CHP_FILE_NAME=os.environ['HOME'] + '/Desktop/CHIP-flasher/flash_me.chp'
 
 #Note that if you download the file with a browser, it will be ready to use. However, if you want to download from the command line,
 #you need to decompress the file explicitly (the browser does this automatically). For example:
